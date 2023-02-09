49c4462a29d11ef38487617906cfd14fc54fe13c
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-02-09 01:53:01 -0500

    test complete

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,25 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Event({ attendees, event, updateEventAttendance }) {
+  console.log(event.id)
+  return (
+    <>
+ <li className="event" key={event.id}>
+              <img src={event.eventImage} alt={event.name} />
+              <h5>
+                {event.name} {event.eventType}
+              </h5>
+              <br />
+              <Attendees
+              event={event}
+
+              
+               attendees={attendees}
+               updateEventAttendance={updateEventAttendance}
+              />
+              <span>Organized by: {event.organizer} </span>
+              <br />
+            </li>
+    </>
+  )
 }
