42ff1935dabb302508452d2b2163b3fe69756939
Felizj17 <felizj17@gmail.com>
2023-02-08 22:22:26 -0500

    lab complete-readme file could be better

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,12 @@
-export default function Attendees() {
-  return;
+import React, {useState} from 'react'
+import Attendee from './Attendee'
+
+export default function Attendees({event, attendees,updateEventAttendance}) {
+  return (
+    <div className='attendees'>
+      {attendees.map((attendee, index) => (
+        <Attendee key={`attendee-${index}`} event={event} attendee={attendee} updateEventAttendance={updateEventAttendance}/>
+      ))}
+    </div>
+  )
 }
