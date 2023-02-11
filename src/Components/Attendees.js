7cd033af53199a7b130fc1f3d3d26cf67fbb4d9a
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-02-11 11:23:01 -0500

    completed Attendee component

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,42 +1,9 @@
-export default function Attendees(updateEventAttendance, attendees, event) {
+import Attendee from "./Attendee"
+export default function Attendees({updateEventAttendance, attendees, event}) {
   return(
     <div className="attendees">
     {attendees.map((attendee, index) => (
-      <>
-        <div key={attendee.id} className="attendee">
-          <p>
-            <img
-              src={attendee.avatar}
-              alt={attendee.firstName}
-            />
-            {"   "}
-            <span>
-              {" "}
-              {attendee.firstName} {attendee.lastName}{" "}
-            </span>
-          </p>
-          <p>
-            <button
-              className="clickable"
-              onClick={() =>
-                updateEventAttendance(
-                  event.id,
-                  attendee.id
-                )
-              }
-            >
-              Attending:
-            </button>
-            <span>
-              {attendee.attendance ? "✅" : "❌"}
-            </span>
-          </p>
-
-          <p>
-            <span>Note:</span> {attendee.note}
-          </p>
-        </div>
-      </>
+      <Attendee updateEventAttendance={updateEventAttendance} attendee={attendee} event />
     ))}
   </div>
   )
