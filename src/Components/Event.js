00870a7eedeb483403062adfdfe78e516dfdbb6a
ninajavier <ninajavier13@gmail.com>
2023-02-12 01:35:26 -0500

    Moved Attendees from Event to Components

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,5 @@
+import Attendees from "./Attendees";
+
 export default function Event() {
   return (
     <li key={event.id}>
@@ -14,45 +16,7 @@ export default function Event() {
                       </button>
 
                       {showAttendees ? (
-                        <div className="attendees">
-                          {attendees.map((attendee, index) => (
-                            <>
-                              <div key={attendee.id} className="attendee">
-                                <p>
-                                  <img
-                                    src={attendee.avatar}
-                                    alt={attendee.firstName}
-                                  />
-                                  {"   "}
-                                  <span>
-                                    {" "}
-                                    {attendee.firstName} {attendee.lastName}{" "}
-                                  </span>
-                                </p>
-                                <p>
-                                  <button
-                                    className="clickable"
-                                    onClick={() =>
-                                      updateEventAttendance(
-                                        event.id,
-                                        attendee.id
-                                      )
-                                    }
-                                  >
-                                    Attending:
-                                  </button>
-                                  <span>
-                                    {attendee.attendance ? "✅" : "❌"}
-                                  </span>
-                                </p>
-
-                                <p>
-                                  <span>Note:</span> {attendee.note}
-                                </p>
-                              </div>
-                            </>
-                          ))}
-                        </div>
+                        <Attendees />
                       ) : null}
                     </>
                   </li>
