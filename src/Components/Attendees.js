d142018d9b6098671d296e00d80d720f9a8756ef
MichelBDLC <michelbdlc@gmail.com>
2023-03-10 23:07:20 -0800

    ...

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,16 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+
+export default function Attendees(props) {
+
+  return (
+    <div className="attendees">
+    {props.showAttendees ? (
+      <div>
+        {props.attendees.map((attendee) => (
+           <Attendee attendee={attendee} event={props.event} updateEventAttendance={props.updateEventAttendance} />
+        ))}
+      </div>
+    ) : null}
+  </div>
+  )
 }
