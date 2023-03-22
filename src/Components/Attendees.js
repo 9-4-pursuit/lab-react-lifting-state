43d6bcd92aebddd5b44e9b391d42a9ed223550f3
Diegocarrero <diegocar2413@gmail.com>
2023-03-22 12:53:53 -0400

    Add Event, Attendees and Attendee components

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,19 @@
+import Attendee from "./Attendee";
+
 export default function Attendees() {
-  return;
+  return (
+    <div>
+      <button onClick={toggleEventAttendees}>
+        {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+      </button>
+
+      <div className="attendees">
+        {attendees.map((attendee, index) => (
+          <>
+            <Attendee/>
+          </>
+        ))}
+      </div>
+    </div>
+  )
 }
