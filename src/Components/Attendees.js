44eef9cfa63dd9eb1a1c8c9bdb391073973b8111
shareekaepps <shareekaepps@icloud.com>
2023-04-04 16:41:03 -0400

    fixed cypress tests running, pass

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,11 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee"
+
+export default function Attendees({ event, updateEventAttendance, attendees }) {
+  return (
+    <div className="attendees">
+      {attendees.map((attendee, index) => (
+        <Attendee updateEventAttendance={updateEventAttendance} attendee={attendee} event={event} key={index} />
+      ))}
+    </div>
+  )
 }
